f092e192d4711744046adde638de9def89ec3959
kermikyu <btran299@csu.fullerton.edu>
2026-02-16 15:08:09 -0800

    Fixed up

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';
+import '../styles/Header.css';
 
 const Header = ({ onLogout, isAuthenticated }) => {
   const [greeting, setGreeting] = useState('Good morning');
