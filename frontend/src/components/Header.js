c298e8dfe4e6ae69e496348ce3a9a782558949a8
jenjentran15 <tranj1215@gmail.com>
2026-03-09 16:02:31 -0700

    new and interactive, will replace some filler stuff later, and added some new styles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-/* Header.js - site navigation bar with time-based greeting and links. */
+
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Header.css';
